5c4bd866b0f836689bcebef7c11638759b70d545
Gabriel <gccomputacao@gmail.com>
2023-07-25 19:17:59 -0300

    Parte 2.1

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,6 @@
 # Trabalhando com funções - def - em Python
 # https://www.youtube.com/watch?v=T0j68JbeGJg&list=PLbIBj8vQhvm0ayQsrhEf-7-8JAj-MwmPr&index=19
-# Parte 1
+# Parte 1 - def
 
 # Passagem de parametros
 def mensagem(msg):
@@ -39,11 +39,13 @@ def saudacaoY(msg='Olá', nome='Gabriel'):
 
 mensagem('Executando a função mensagem')
 saudacao()
+
+# Passagem de parametros fora de ordem na chamada da funçao
 saudacao(nome='José', msg='Oi')
 saudacaoX()
 print("saudacaoY:", saudacaoY())
 
-# Parte 2
+# Parte 2 - def e return
 
 x = input("Digite dois numeros: ")
 x = x.split(' ')
@@ -58,9 +60,19 @@ def divisao(n1, n2):
     return
 
 
-divide = divisao(x[0], x[1])
+a = divisao
+divide = a(x[0], x[1])
+
+print(f"a = {a}\
+      \nid(a) = {id(a)}\
+      \nid(divisao) = {id(divisao)}\
+      \ntipo(a) = {type(a)}\
+      \ntipo(divisao) = {type(divisao)}")
 
 if divide:
     print(divide)
 else:
     print("Divisão por zero!")
+
+
+# Parte 3 *args e **kwargs
